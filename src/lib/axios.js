97c42ba7ae7186f7941b87b3636945c9c982ad6b
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2026-01-21 16:39:44 +0530

    commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = "https://asset-backend-slqs.onrender.com/api";
 
 // Public axios (for login/register)
 export const axiosPublic = axios.create({
